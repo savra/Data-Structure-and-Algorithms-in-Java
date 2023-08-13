007478dd07cbb00235316d9ef4fb85be7d2b2fe2
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-13 11:39:43 +0300

    Initial commit

---
+++ src/test/java/com/example/datastructureandalgorithms/DataStructureAndAlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datastructureandalgorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructureAndAlgorithmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
