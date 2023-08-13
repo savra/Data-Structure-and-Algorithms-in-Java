f7dbb0b7e4745b9526f15b7a33299f69cd0d33b6
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-13 11:40:14 +0300

    Initial commit

--- src/main/java/com/example/datastructureandalgorithms/DataStructureAndAlgorithmsApplication.java
+++ src/main/java/com/example/datastructureandalgorithms/DataStructureAndAlgorithmsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DataStructureAndAlgorithmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DataStructureAndAlgorithmsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DataStructureAndAlgorithmsApplication.class, args);
+    }
 
 }
