b6de5deca1283a44150544e173cd0b83caaf8191
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-22 21:03:20 +0300

    Euclidean algorithm

--- src/main/java/com/hvdbs/savra/datastructureandalgorithms/java/Math/EuclideanAlgorithm.java
+++ src/main/java/com/hvdbs/savra/datastructureandalgorithms/java/Math/EuclideanAlgorithm.java
@@ -1,5 +1,7 @@
 package com.hvdbs.savra.datastructureandalgorithms.java.Math;
 
+import org.springframework.util.Assert;
+
 public class EuclideanAlgorithm {
     private static int euclideanAlgorithm(int dividend, int divider) {
         if (divider == 0) {
@@ -7,5 +9,11 @@ public class EuclideanAlgorithm {
         }
 
         return euclideanAlgorithm(divider, dividend % divider);
+    }
+
+    public static void main(String[] args) {
+        Assert.isTrue(euclideanAlgorithm(106,16) == 2, "Assertion error");
+        Assert.isTrue(euclideanAlgorithm(108,72) == 36, "Assertion error");
+        Assert.isTrue(euclideanAlgorithm(44,60) == 4, "Assertion error");
     }
 }
